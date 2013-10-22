df61fb3c04b382f25433316b59052864649a3a55
Josiah Wang <ysjwang@gmail.com>
2013-10-22 13:12:53 -0400

    Changed indentations to be 2 spaces

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,32 +1,32 @@
 class AccountsController < ApplicationController
-	before_filter :authenticate_user!
-	def new
-		@account = Account.new
-	end
+  before_filter :authenticate_user!
+  def new
+    @account = Account.new
+  end
 
-	def create
-		@account = current_user.accounts.build(params[:account])
+  def create
+    @account = current_user.accounts.build(params[:account])
 
-		if @account.save
-			#redirect_to accounts_path, :success => "Account successfully created."
-			flash[:success] = "Account sucessfully created."
-			@accounts = current_user.accounts
-		else
-			flash[:error] = "There was a problem with adding your account."
-			#render 'new'
-		end
-	end
+    if @account.save
+      #redirect_to accounts_path, :success => "Account successfully created."
+      flash[:success] = "Account sucessfully created."
+      @accounts = current_user.accounts
+    else
+      flash[:error] = "There was a problem with adding your account."
+      #render 'new'
+    end
+  end
 
-	def show
-		@account = Account.find_by_id(params[:id])
-		@debit_transactions = @account.debit_transactions.all
-		@credit_transactions = @account.credit_transactions.all
+  def show
+    @account = Account.find_by_id(params[:id])
+    @debit_transactions = @account.debit_transactions.all
+    @credit_transactions = @account.credit_transactions.all
 
-		@transactions = (@debit_transactions + @credit_transactions).uniq
-		@transactions.sort! { |a,b| b.created_at <=> a.created_at }
-	end
+    @transactions = (@debit_transactions + @credit_transactions).uniq
+    @transactions.sort! { |a,b| b.created_at <=> a.created_at }
+  end
 
-	def index
-		@accounts = current_user.accounts.all
-	end
+  def index
+    @accounts = current_user.accounts.all
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,20 @@
 module ApplicationHelper
-  
+
   def stylized_flash(key)
-      case key
-        when :alert
-          "alert-warning"
-        when :error
-          "alert-danger"
-        when :notice
-          "alert-info"
-        when :success
-          "alert-success"
-        else
-          # "alert-" + key.to_s
-          "alert-info" # just use default info for now.
-      end
+    case key
+    when :alert
+      "alert-warning"
+    when :error
+      "alert-danger"
+    when :notice
+      "alert-info"
+    when :success
+      "alert-success"
+    else
+      # "alert-" + key.to_s
+      "alert-info" # just use default info for now.
     end
-    
-  
+  end
+
+
 end
